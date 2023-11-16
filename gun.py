e355105ae1e79e4f6ecded452ce63447763ac678
Dungeon_M. <novokhatnii.aa@phystech.edu>
2023-11-17 01:02:46 +0300

    Added bullet amount mechanics

--- gun.py
+++ gun.py
@@ -16,11 +16,14 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     bullet = 0
     balls = []
+    bullets_amount = 10
 
     gun_image.convert_alpha()
     gun_image_flip.convert_alpha()
     background_image.convert_alpha()
     target_image.convert_alpha()
+    bullet_image.convert_alpha()
+    bullet_image = pygame.transform.rotozoom(bullet_image, 0, 0.02)
 
     clock = pygame.time.Clock()
     gun = Gun(screen)
@@ -32,13 +35,21 @@ if __name__ == "__main__":
         gun.draw()
         if target.live:
             target.draw()
-        draw_text(screen, 'Your score: ' + str(target.points), 18, WIDTH / 2, 10)
         for b in balls:
             b.draw()
+
         power_bar = PowerBar(screen, gun.x, gun.y, gun.f2_power)
         pb_ratio = power_bar.ratio()
         pb_color = power_bar.color(pb_ratio)
         power_bar.draw(pb_color, pb_ratio, gun.f2_on)
+
+        draw_text(screen, 'Your score: ' + str(target.points), 18, WIDTH / 2, 10)
+        screen.blit(bullet_image, bullet_image.get_rect(center=(20, 30)))
+        draw_text(screen, str(bullets_amount), 25, 50, 20)
+        if bullets_amount <= 0:
+            draw_text(screen, 'GAME OVER! SCORE: ' + str(target.points), 40, WIDTH / 2, HEIGHT / 2 - 110)
+            draw_text(screen, 'PRESS R TO RESTART', 40, WIDTH / 2, HEIGHT / 2 - 60)
+
         pygame.display.update()
         target.move()
 
@@ -46,18 +57,21 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finished = True
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.MOUSEBUTTONDOWN and bullets_amount > 0:
                 gun.fire2_start(event)
-            elif event.type == pygame.MOUSEBUTTONUP:
+            elif event.type == pygame.MOUSEBUTTONUP and bullets_amount > 0:
                 gun.fire2_end(event, balls, bullet)
+                bullets_amount -= 1
 
         key_pressed = pygame.key.get_pressed()
         if key_pressed[pygame.K_ESCAPE]:
             finished = True
-        if key_pressed[pygame.K_LEFT]:
+        if key_pressed[pygame.K_LEFT] or key_pressed[pygame.K_a]:
             gun.move(-5)
-        if key_pressed[pygame.K_RIGHT]:
+        if key_pressed[pygame.K_RIGHT] or key_pressed[pygame.K_d]:
             gun.move(5)
+        if bullets_amount <= 0 and key_pressed[pygame.K_r]:
+            bullets_amount = 10
         gun.targetting(gun.get_mouse_position())
 
         for b in balls:
@@ -66,6 +80,7 @@ if __name__ == "__main__":
                 target.live = 0
                 target.hit()
                 t = pygame.time.get_ticks()
+                bullets_amount += 2
             if target.live == 0 and pygame.time.get_ticks() - t >= 1000:
                 target.new_target()
             if abs(b.get_vx()) <= 1e-20:
